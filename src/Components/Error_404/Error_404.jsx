bd5a895bb6e6bddf1ec11579ad7a96fc20df7417
Mohamed Hisham <d.mohamedtarek12@gmail.com>
2025-04-18 23:12:43 +0200

    Finish the base of all components

---
+++ src/Components/Error_404/Error_404.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Error_404() {
+  return <div>Error_404</div>;
+}
---
+++ src/Components/MediaDetails/MediaDetails.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MediaDetails() {
+  return <div>MediaDetails</div>;
+}
