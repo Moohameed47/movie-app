6e3c6979146d2df4fe1ab3ac6cf06830be34c439
Mohamed Hisham <d.mohamedtarek12@gmail.com>
2025-04-19 00:59:09 +0200

    finish Tv Component

--- src/Components/Tv/Tv.jsx
+++ src/Components/Tv/Tv.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Media from "../Media/Media";
 
 export default function Tv() {
-  return <div>Tv</div>;
+  const [tv, settv] = useState([]);
+  async function getAPI(type, setdata) {
+    const { data } = await Axios.get(
+      `https://api.themoviedb.org/3/trending/${type}/day?api_key=8494c70d32335f06e505959598e8ff8c`
+    );
+    console.log(data.results);
+
+    // CallBackFunction \\
+    setdata(await data.results);
+  }
+
+  useEffect(() => {
+    getAPI("tv", settv);
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="row my-5 justify-content-center text-center">
+          <div className="col-md-4">
+            <div className="brdr w-25 mx-auto"></div>
+
+            <h2 className="text-uppercase">
+              <Link className="link" to="/tv">
+                Trending Tv
+              </Link>
+            </h2>
+
+            <div className="brdr w-75 mx-auto"></div>
+            <p className="h5 fw-light mt-3">Most watched series to watch now</p>
+          </div>
+        </div>
+        <div className="row my-2">
+          {tv.length > 0
+            ? tv.map((el) => <Media data={el} wdth="col-md-2" />)
+            : ""}
+        </div>
+      </div>
+    </>
+  );
 }
