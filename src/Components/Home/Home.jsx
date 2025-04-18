607a77d2b42618f48305d8eff0df288f504788fd
Mohamed Hisham <d.mohamedtarek12@gmail.com>
2025-04-18 23:47:11 +0200

    fetch data from TMDB

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const API = "https://api.themoviedb.org/3/trending/movie/day";
+  const API_KEY = "8494c70d32335f06e505959598e8ff8c";
+  const [movies, setmovies] = useState([]);
+  const [tv, settv] = useState([]);
+  const [people, setpeople] = useState([]);
+  async function getAPI(type, setdata) {
+    const { data } = await Axios.get(
+      `https://api.themoviedb.org/3/trending/${type}/day?api_key=8494c70d32335f06e505959598e8ff8c`
+    );
+    // CallBackFunction \\
+    setdata(await data);
+  }
+
+  useEffect(() => {
+    getAPI("movie", setmovies);
+    getAPI("tv", settv);
+    getAPI("person", setpeople);
+  }, []);
+
+  return (
+    <>
+      <div>{movies.total_results}</div>
+      <div>{tv.total_results}</div>
+      <div>{people.total_results}</div>
+    </>
+  );
 }
